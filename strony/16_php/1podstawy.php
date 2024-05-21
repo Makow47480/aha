e91ea4ad185835f52461f3252c82e3d16ecb219c
Makow47480 <160600125+Makow47480@users.noreply.github.com>
2024-05-21 09:24:17 +0200

    Update 1podstawy.php

--- strony/16_php/1podstawy.php
+++ strony/16_php/1podstawy.php
@@ -3,9 +3,22 @@
 <title>mój pierwszy skrypt php</title>
 </head>
 <body>
-<?php
+<?php 
 // Skrypt ten wypisze tekst, uŻywając funkcji "print"
 print("PHP");
 ?>
 </body>
 </html>
+echo ('Zaczynam poznawać PHP');
+echo ("Zaczynam poznawać PHP");
+print ('Zaczynam poznawać PHP');
+print ("Zaczynam poznawać PHP");
+print ("<br>");
+echo ('Zaczynam poznawać PHP');
+print ("<br>");
+echo ("Zaczynam poznawać PHP");
+print ("<br>");
+print ('Zaczynam poznawać PHP');
+print ("<br>");
+print ("Zaczynam poznawać PHP");
+print ("Zaczynam poznawać PHP. "."Lubię PHP.");
